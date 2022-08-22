ea15f21def74f20e8d1efc4b24fc3c513801f243
Vu Duy Nhien <vuduynhiencute@gmail.com>
2022-08-22 18:32:45 +0700

    config prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
-function App() { 
-  return (
-    <div className="App">
-    
-    </div>
-  );
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
